fbb5de7eb53173d0f8c2e3a533cc2841b1461ab7
S77VEN31 <stivensg313131@outlook.com>
2023-08-04 02:22:34 -0700

    hotfix: route NavBar bug

--- app/components/NavBars/NavBarMenu/NavBarMenu.tsx
+++ app/components/NavBars/NavBarMenu/NavBarMenu.tsx
@@ -1,6 +1,6 @@
 'use client';
 // React
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // Next
 import Link from 'next/link';
 // Global
@@ -9,7 +9,16 @@ import { classes } from '@/global/handleClassnames';
 import './NavBarMenu.style.css';
 
 const NavBarMenu: React.FC = () => {
-  const [option, setOption] = useState('home');
+  const [option, setOption] = useState(() => {
+    // Intenta obtener el valor de 'option' desde localStorage al cargar el componente
+    const savedOption = localStorage.getItem('selectedOption');
+    return savedOption ? savedOption : 'home'; // Si no hay valor, utiliza 'home' como valor predeterminado
+  });
+
+  useEffect(() => {
+    // Almacena el estado 'option' en localStorage cada vez que cambie
+    localStorage.setItem('selectedOption', option);
+  }, [option]);
   const options = [
     {
       children: 'aruba',
